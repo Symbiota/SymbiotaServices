edd2e204dfa6e1b5957aba8d86740e8c4d5fdc43
atticus29 <mark.aaron.fisher@gmail.com>
2025-06-26 19:18:42 -0700

    add customer-management view and a layout component. Implement tailwind in app.css

---
+++ resources/views/customer-management.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+   <h1 class="text-white">Test!</h1> 
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/customer-management', function () {
+    return view('customer-management');
+});
