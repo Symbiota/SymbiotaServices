7bee99c51284caaed73e6d4b8a0be33a947c2e93
atticus29 <mark.aaron.fisher@gmail.com>
2025-07-01 12:28:40 -0700

    minor aesthetics fixes to font colors

--- resources/views/customer-management.blade.php
+++ resources/views/customer-management.blade.php
@@ -8,7 +8,7 @@
 
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div class="sm:col-span-4">
-               <label for="customer-name" class="block text-sm/6 font-medium text-gray-900">Customer (institution) Name</label>
+               <label for="customer-name" class="block text-sm/6 font-medium text-900 text-white">Customer (institution) Name</label>
                <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                   <input type="text" name="customer-name" id="customer-name" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="University of Kansas" />
@@ -16,7 +16,7 @@
                </div>
             </div>
             <div class="sm:col-span-4">
-               <label for="customer-DARBI-number" class="block text-sm/6 font-medium text-gray-900">Customer DARBI Number</label>
+               <label for="customer-DARBI-number" class="block text-sm/6 font-medium text-900 text-white">Customer DARBI Number</label>
                <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                   <input type="text" name="customer-DARBI-number" id="customer-DARBI-number" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="1234" />
@@ -31,8 +31,7 @@
       </div>
 
       <div class="mt-6 flex items-center justify-end gap-x-6">
-         <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-         <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+         <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
       </div>
    </form>
 </x-layout>
