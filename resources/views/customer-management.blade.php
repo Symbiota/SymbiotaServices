4596842be0c601dbfea2ac9fa13554d3b92131d6
atticus29 <mark.aaron.fisher@gmail.com>
2025-07-02 10:24:54 -0700

    add form validation and error display

--- resources/views/customer-management.blade.php
+++ resources/views/customer-management.blade.php
@@ -11,7 +11,7 @@
                <label for="customer-name" class="block text-sm/6 font-medium text-900 text-white">Customer (institution) Name</label>
                <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <input type="text" name="customer-name" id="customer-name" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="e.g., University of Kansas" />
+                  <input type="text" name="customer-name" id="customer-name" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="e.g., University of Kansas" required />
                   </div>
                </div>
             </div>
@@ -19,7 +19,7 @@
                <label for="customer-DARBI-number" class="block text-sm/6 font-medium text-900 text-white">Customer DARBI Number</label>
                <div class="mt-2">
                   <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                  <input type="text" name="customer-DARBI-number" id="customer-DARBI-number" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="e.g., 1234" />
+                  <input type="text" name="customer-DARBI-number" id="customer-DARBI-number" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="e.g., 1234" required />
                   </div>
                </div>
             </div>
@@ -28,5 +28,12 @@
       <div class="mt-6 flex items-center justify-end gap-x-6">
          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Create</button>
       </div>
+      @if($errors->any())
+         <ul>
+            @foreach($errors->any() as $error)
+               <li>{{$error}}</li>
+            @endforeach
+         </ul>
+      @endif
    </form>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,16 @@ Route::get('/customer-management', function () {
 
 Route::post('/customer', function(){
     //@TODO validate
+    request()->validate([
+        'customer-name'=>['required'],
+        'customer-DARBI-number'=>['required']
+
+    ]);
     Customer::create([
         'name'=>request('customer-name'),
         'darbi_account'=>request('customer-DARBI-number')
     ]);
-
-    // @TODO return what? a redirect?
-    // dd(request()->all());
+    return redirect('/customer-management');
 })
 
 ?>
