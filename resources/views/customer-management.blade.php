5d467fe9dadd3611cd05107c17bff3c47403d4ed
atticus29 <mark.aaron.fisher@gmail.com>
2025-07-07 10:35:21 -0700

    add TODO

--- resources/views/customer-management.blade.php
+++ resources/views/customer-management.blade.php
@@ -45,6 +45,7 @@
                      </div>
                   </div>
                </div>
+               <!-- TODO Add notes field -->
             </div>
          </div>
          <div class="mt-6 flex items-center justify-end gap-x-6">
