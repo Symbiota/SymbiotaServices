cac8c96499172016fe9a5dc18954c342bbd6c95f
atticus29 <mark.aaron.fisher@gmail.com>
2025-07-15 14:12:11 -0700

    remove alpine from bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 import htmx from "htmx.org";
-import Alpine from "alpinejs";
 
 window.axios = axios;
 window.htmx = htmx;
-window.Alpine = Alpine;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-queueMicrotask(() => {
-    Alpine.start();
-});
