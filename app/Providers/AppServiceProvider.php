2b9306260a485ea7acb25f4b330ad51277d573b9
atticus29 <mark.aaron.fisher@gmail.com>
2025-07-15 17:18:34 -0700

    remove more cruft and auto-format some stuff

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Livewire\Component;
 
 class AppServiceProvider extends ServiceProvider
 {
--- routes/web.php
+++ routes/web.php
@@ -12,36 +12,31 @@ Route::get('/customer-management', [CustomerController::class, 'index']);
 Route::post('/customer', [CustomerController::class, 'create']);
 Route::delete('/customer/{id}', [CustomerController::class, 'destroy']);
 
-
-
-
-Route::get('/contracts', function (){
+Route::get('/contracts', function () {
     return view('contracts', ['contracts' => Contract::all()]);
 });
 
-Route::get('/contracts/{id}', function ($id){
+Route::get('/contracts/{id}', function ($id) {
     $contract = Contract::find($id);
     return view('contract', compact('contract'));
 });
 
-
-Route::get('/customers', function (){
+Route::get('/customers', function () {
     return view('customers.index', ['customers' => Customer::all()]);
 });
-
 
 Route::get('/customers/{id}', function ($id) {
     $customer = Customer::find($id);
     return view('customers.show', compact('customer')); // = ['customer' => $customer]
 });
 
-Route::get('/customers/{id}/edit', function ($id){
+Route::get('/customers/{id}/edit', function ($id) {
     $customer = Customer::find($id);
     return view('customers.edit', ['customer' => $customer]);
 });
 
 // Update
-Route::patch('/customers/{id}', function ($id){
+Route::patch('/customers/{id}', function ($id) {
     // Validate
     request()->validate([
         'name' => ['required'],
@@ -58,7 +53,7 @@ Route::patch('/customers/{id}', function ($id){
         'darbi_account' => request('darbi_account'),
         'darbi_site' => request('darbi_site'),
         'correspondence' => request('correspondence'),
-        'notes' => request('notes')
+        'notes' => request('notes'),
     ]);
 
     // and Persist
@@ -66,32 +61,5 @@ Route::patch('/customers/{id}', function ($id){
     // redirect to the customer page
     return redirect('/customers/' . $customer->id);
 });
-
-
-
-
-// Create
-// Route::post('/customers', function () {
-
-//     request()->validate([
-//         'name' => ['required'],
-//         'darbi_account' => ['required', 'numeric', 'digits:4'],
-//         'darbi_site' => ['required'],
-//         'correspondence' => ['required'],
-//     ]);
-
-//     Customer::create([
-//         'name' => request('name'),
-//         'darbi_account' => request('darbi_account'),
-//         'darbi_site' => request('darbi_site'),
-//         'correspondence' => request('correspondence'),
-//         'notes' => request('notes')
-//     ]);
-
-//     return redirect('/customers');
-// });
-
-
-
 
 ?>
