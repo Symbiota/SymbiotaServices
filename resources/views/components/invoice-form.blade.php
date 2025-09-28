8c1064a8874c836c860e1f1acb05e4276d45e56b
Skylar Franz <skylar.franz@ku.edu>
2025-09-27 20:18:01 -0500

    Make blade component for input selection

--- resources/views/components/invoice-form.blade.php
+++ resources/views/components/invoice-form.blade.php
@@ -16,39 +16,35 @@
             </x-form-box>
 
             <x-form-box for="financial_contact_id"> Financial Contact ID*
-                <div
-                    class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 mt-2">
-                    <select name="financial_contact_id"
-                        id="financial_contact_id"
-                        class="bg-white block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 border border-gray-500">
-                        @isset($invoice)
-                            // On invoice edit page
-                            <option value="{{ $invoice->financial_contact_id }}">
-                                {{ $invoice->financial_contact->last_name }},
-                                {{ $invoice->financial_contact->first_name }}
-                                - {{ $invoice->financial_contact_id }}
-                            </option>
-                        @endisset
-                        @isset($contract->current_financial_contact_id)
-                            // From contract view to invoice creation, autofill
-                            current financial contact
-                            <option
-                                value="{{ $contract->current_financial_contact_id }}">
-                                {{ $contract->current_financial_contact->last_name }},
-                                {{ $contract->current_financial_contact->first_name }}
-                                - {{ $contract->current_financial_contact_id }}
-                            </option>
-                        @endisset
-                        <option value=""></option>
-                        @foreach ($contacts as $contact)
-                            <option value="{{ $contact->id }}">
-                                {{ $contact->last_name }},
-                                {{ $contact->first_name }} -
-                                {{ $contact->id }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+                <x-form-selection-input name="financial_contact_id"
+                    id="financial_contact_id">
+                    @isset($invoice)
+                        // On invoice edit page
+                        <option value="{{ $invoice->financial_contact_id }}">
+                            {{ $invoice->financial_contact->last_name }},
+                            {{ $invoice->financial_contact->first_name }}
+                            - {{ $invoice->financial_contact_id }}
+                        </option>
+                    @endisset
+                    @isset($contract->current_financial_contact_id)
+                        // From contract view to invoice creation, autofill
+                        current financial contact
+                        <option
+                            value="{{ $contract->current_financial_contact_id }}">
+                            {{ $contract->current_financial_contact->last_name }},
+                            {{ $contract->current_financial_contact->first_name }}
+                            - {{ $contract->current_financial_contact_id }}
+                        </option>
+                    @endisset
+                    <option value=""></option>
+                    @foreach ($contacts as $contact)
+                        <option value="{{ $contact->id }}">
+                            {{ $contact->last_name }},
+                            {{ $contact->first_name }} -
+                            {{ $contact->id }}
+                        </option>
+                    @endforeach
+                </x-form-selection-input>
                 @error('financial_contact_id')
                     <p class="text-red-500 text-sm ml-3">
                         {{ $message }}
