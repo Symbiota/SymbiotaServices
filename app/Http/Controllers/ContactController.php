6f52762faf5eaa43a0d2ff09b5c4d66bed480024
Skylar Franz <skylar.franz@ku.edu>
2025-11-22 21:50:22 -0600

    Refactor Contacts show/edit to use HTMX modals

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ContactController extends Controller
 {
@@ -12,9 +13,12 @@ class ContactController extends Controller
         return view('contacts.index', ['contacts' => Contact::all()]);
     }
 
-    public function show(Contact $contact)
+    public function show(Request $request, Contact $contact)
     {
-        return view('contacts.show', ['contact' => $contact]);
+        $isHTMX = $request->hasHeader('HX-Request');
+
+        return view('contacts.show', compact('contact', 'isHTMX'))
+            ->fragmentIf($isHTMX, 'show-contact');
     }
 
     public function store(Request $request)
@@ -34,16 +38,27 @@ class ContactController extends Controller
 
     public function update(Request $request, Contact $contact)
     {
-        $data = $request->validateWithBag('contact_errors', [
-            'first_name' => ['required'],
-            'last_name' => ['required'],
-            'email' => ['required', 'email'],
-            'phone_number' => ['nullable'],
-            'notes' => ['nullable'],
-        ]);
+        $isHTMX = $request->hasHeader('HX-Request');
 
-        $contact->update($data);
+        try {
+            $data = $request->validateWithBag('contact_errors', [
+                'first_name' => ['required'],
+                'last_name' => ['required'],
+                'email' => ['required', 'email'],
+                'phone_number' => ['nullable'],
+                'notes' => ['nullable'],
+            ]);
 
-        return redirect()->route('contacts.show', $contact);
+            $contact->update($data);
+
+            return view('contacts.show', compact('contact', 'isHTMX'))->fragmentIf($isHTMX, 'show-contact');
+
+        } catch (ValidationException $e) {
+            if ($isHTMX) {
+                return view('contacts.show', compact('contact', 'isHTMX'))->withErrors($e->errors(), 'contact_errors')->fragment('show-contact');
+            }
+            throw $e;
+        }
+
     }
 }
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -19,7 +19,10 @@
     <div class = "space-y-4">
         @foreach ($contacts as $contact)
             <a href="{{ route('contacts.show', $contact) }}"
-                class="block px-4 py-6 border border-gray-500">
+                class="block px-4 py-6 border border-gray-500"
+                hx-get="{{ route('contacts.show', $contact) }}"
+                hx-target="#modal" hx-swap="innerHTML"
+                onclick="toggleView('modal-container')">
                 <ul>
                     <li><b>Contact ID:</b> {{ $contact->id }}</li>
                     <li><b>Name:</b> {{ $contact->first_name }}
