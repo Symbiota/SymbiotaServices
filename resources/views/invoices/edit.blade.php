45acd272bf316d72ce4adf50ef2dcf25d0b398b6
Skylar Franz <skylar.franz@ku.edu>
2025-11-28 20:26:20 -0600

    Fix Invoice modal header

--- resources/views/invoices/edit.blade.php
+++ resources/views/invoices/edit.blade.php
@@ -1,7 +1,7 @@
 <x-table-layout heading="Edit Invoice {{ $invoice->id }}:">
     @fragment('edit-invoice')
         <x-modal-header :isHTMX="$isHTMX" :errors="$errors" model="Invoice">Edit
-            Invoice{{ $invoice->id }}:</x-modal-header>
+            Invoice {{ $invoice->id }}:</x-modal-header>
         @if ($isHTMX)
             <x-invoice-form hx-post="{{ route('invoices.update', $invoice) }}"
                 hx-target="#modal" hx-swap="innerHTML scroll:top" :errors="$errors"
