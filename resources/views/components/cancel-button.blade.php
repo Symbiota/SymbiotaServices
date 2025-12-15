2a12454544f9b623013b7889ff3087fbb6d61edc
Skylar Franz <skylar.franz@ku.edu>
2025-12-15 11:28:33 -0600

    Add Button Text

--- resources/views/components/cancel-button.blade.php
+++ resources/views/components/cancel-button.blade.php
@@ -1,9 +1,9 @@
 @if (request()->routeIs('services.show') || request()->routeIs('contracts.show'))
     <button type="button" class="text-sm/6 font-semibold text-gray-900"
-        onclick="toggleView('edit-form');"></button>
+        onclick="toggleView('edit-form');">Cancel</button>
 @elseif (request()->hasHeader('HX-Request'))
     <button type="button" class="text-sm/6 font-semibold text-gray-900"
-        onclick="toggleView('modal-container');"></button>
+        onclick="toggleView('modal-container');">Cancel</button>
 @else
     <a class="text-sm/6 font-semibold text-gray-900"
         href="{{ url()->previous() }}">Cancel</a>
