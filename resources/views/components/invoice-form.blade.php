c0737d69faf911045ab6f3d0740e4be00256ab18
Skylar Franz <skylar.franz@ku.edu>
2025-12-15 12:38:40 -0600

    Invoice form cancel button

--- resources/views/components/invoice-form.blade.php
+++ resources/views/components/invoice-form.blade.php
@@ -166,7 +166,15 @@
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
 
-        <x-cancel-button></x-cancel-button>
+        <x-cancel-button>
+            @if (request()->routeIs('invoices.edit'))
+                {{ route('invoices.show', $invoice) }}
+            @elseif (request()->routeIs('invoices.create') && !empty($contract->id))
+                {{ route('contracts.show', $contract) }}
+            @elseif (request()->routeIs('invoices.create'))
+                {{ route('invoices.index') }}
+            @endif
+        </x-cancel-button>
 
         <button type="submit"
             class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Submit</button>
