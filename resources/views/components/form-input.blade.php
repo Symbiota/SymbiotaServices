9d3611f6893373769c44962041c4b414713c4709
Skylar Franz <skylar.franz@ku.edu>
2025-12-27 09:19:49 -0700

    Pass HTML type="password" to component form-input (#136)
    + Previously, form-input defaulted to type="text" when there was anything other than type="select", causing the password field to fail.

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -10,7 +10,7 @@
                 {{ $slot }}
             </select>
         @else
-            <input
+            <input type="{{ $type }}"
                 {{ $attributes->merge(['class' => 'block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 border border-gray-500']) }} />
         @endisset
 </div>
