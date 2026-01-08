98e05ccf43fde4c971019beb40003f4e23d97fec
Skylar Franz <skylar.franz@ku.edu>
2026-01-08 16:13:43 -0700

    Replace search button icon with text "Search"

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -14,7 +14,7 @@
                     class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-300 dark:focus:border-blue-700 dark:active:bg-gray-700 dark:active:text-gray-300"
                     type="text" name="search" placeholder="Search Customers"
                     list="customer-datalist">
-                <x-ec-button class="!ml-1">⌕</x-ec-button>
+                <x-ec-button class="!ml-1">Search</x-ec-button>
             </form>
         </div>
 
