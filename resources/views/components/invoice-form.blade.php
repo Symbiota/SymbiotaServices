5db5a94390100ca3033268c4146f10699f274c78
Skylar Franz <skylar.franz@ku.edu>
2026-01-12 12:30:12 -0600

    Show old line refs when creating new invoice

--- resources/views/components/invoice-form.blade.php
+++ resources/views/components/invoice-form.blade.php
@@ -109,13 +109,13 @@
                             class="m-1 mt-2 p-1 border border-gray-500 ml-4"
                             name="line_ref_1[{{ $service->id }}]"
                             id="line_ref_1" placeholder="Line Ref 1"
-                            @if (isset($invoice)) value="{{ $invoice->services->find($service)->pivot->line_ref_1 ?? '' }}"
+                            @if (!empty($invoice->id)) value="{{ $invoice->services->find($service)->pivot->line_ref_1 ?? '' }}"
                             @else value="{{ old('line_ref_1.' . $service->id) }}" @endif>
                         <input type="text"
                             class="m-1 mt-2 p-1 border border-gray-500"
                             name="line_ref_2[{{ $service->id }}]"
                             id="line_ref_2" placeholder="Line Ref 2"
-                            @if (isset($invoice)) value="{{ $invoice->services->find($service)->pivot->line_ref_2 ?? '' }}"
+                            @if (!empty($invoice->id)) value="{{ $invoice->services->find($service)->pivot->line_ref_2 ?? '' }}"
                             @else value="{{ old('line_ref_2.' . $service->id) }}" @endif">
                     </div>
                 @endforeach
