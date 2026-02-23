bee2bea7b305d264d2140a2668c2e2d424c4372c
Skylar Franz <skylar.franz@mailbox.org>
2026-02-23 13:40:28 -0600

    Update form-box depending on modal request

--- resources/views/components/form-box.blade.php
+++ resources/views/components/form-box.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-8 grid grid-cols-6 gap-x-6 gap-y-8">
     <div
-        class="{{ str_contains(request()->url(), 'edit') ? 'col-span-6' : 'col-span-5' }}">
+        class="{{ request()->hasHeader('HX-Request') ? 'col-span-6' : 'col-span-5' }}">
         <label
             {{ $attributes->merge(['class' => 'block text-sm/6 font-medium']) }}>{{ $slot }}</label>
     </div>
