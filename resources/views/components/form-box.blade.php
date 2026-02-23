977652768c87065a75cf95e252a4f3e02df2f77e
Skylar Franz <skylar.franz@mailbox.org>
2026-02-23 13:34:19 -0600

    Edit modals now use full space
    + Previously, edit modal inputs were spanning 5/6's the grid

--- resources/views/components/form-box.blade.php
+++ resources/views/components/form-box.blade.php
@@ -1,5 +1,6 @@
-<div class="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-    <div class="sm:col-span-5">
+<div class="mt-8 grid grid-cols-6 gap-x-6 gap-y-8">
+    <div
+        class="{{ str_contains(request()->url(), 'edit') ? 'col-span-6' : 'col-span-5' }}">
         <label
             {{ $attributes->merge(['class' => 'block text-sm/6 font-medium']) }}>{{ $slot }}</label>
     </div>
