34a62b08e6da2d5825a364ae8543796c92ac25ec
Skylar Franz <skylar.franz@mailbox.org>
2026-07-02 13:11:47 -0500

    Update/fix customer form

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,5 +1,5 @@
 <x-table-layout heading="Create Customer:">
     <title>Create Customer - SymbiotaServices</title>
     <x-forms.customer-form class="-mt-8"
-        action="{{ route('customers.store') }}"></x-forms.customer-form
-        </x-table-layout>
+        action="{{ route('customers.store') }}"></x-forms.customer-form>
+</x-table-layout>
